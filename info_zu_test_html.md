93afc413ac7d1e9d673797f88c94497f6cb3e7cc
Ali <ali.hamid9119@gmail.com>
2022-11-07 14:03:47 +0100

    Design ist fast fertig.

--- info_zu_test_html.md
+++
@@ -1,1 +0,0 @@
-test.html dient als Repräsentation des Games-layouts. Die .html - dateien im Ordner Games connecten nicht richtig mit den links, die ich zuordne. Um mein CSS ist zu machen. existiert test.html.
---
+++ js/header.js
@@ -0,0 +1,7 @@
+let header_games = document.createElement("header");
+
+if(header_main == true){
+    window.location = "/Users/aliha/Desktop/GIS/GIS_videogame_database";
+} else {
+    window.location = "/Users/aliha/Desktop/GIS/GIS_videogame_database/games";
+}
